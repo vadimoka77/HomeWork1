fbd77bebccf944c18d69b05d22058ef697af0bc5
vadimoka77 <59016347+vadimoka77@users.noreply.github.com>
2020-05-20 22:37:19 +0300

    Initial Commit

---
+++ HomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork
+//
+//  Created by Вадим Чариков on 20.05.2020.
+//  Copyright © 2020 Вадим Чариков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
